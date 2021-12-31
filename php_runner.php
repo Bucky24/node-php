0c6498d326c32418fa58f6aba2644db3e0f95f85
Bucky24 <rwijtman@gmail.com>
2021-12-31 13:06:19 -0800

    Better fatal error logging

--- php_runner.php
+++ php_runner.php
@@ -1,12 +1,28 @@
 <?php
+
+    function fatal_handler() {
+        $error = error_get_last();
+
+        if($error !== NULL) {
+            $errno   = $error["type"];
+            $errfile = $error["file"];
+            $errline = $error["line"];
+            $errstr  = $error["message"];
+
+            fwrite(STDERR, "Error on line $errline in file $errfile: $errstr");
+        }
+    }
+
+    register_shutdown_function("fatal_handler");
+
     $file = $argv[1];
     
     $data = file_get_contents($file);
     $data = json_decode($data, true);
     
     //print_r($data);
-	
-	chdir($data['baseDirectory']);
+
+    chdir($data['baseDirectory']);
     
     if (array_key_exists("query", $data)) {
         foreach ($data['query'] as $key=>$value) {
@@ -29,8 +45,8 @@
     if (array_key_exists("files", $data) && $data['files'] !== null) {
         $_FILES = $data['files'];
     }
-	
-	if (!function_exists("getallheaders")) {
+
+    if (!function_exists("getallheaders")) {
 		function getallheaders() {
 			global $data;
 			return $data['headers'];
